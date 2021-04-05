675e5ac55f70dd2f40aba46f65cbf109ca1400bc
Руслан <ryslan.shuyski@mail.ru>
2021-04-05 11:03:21 +0300

    1. Генератор текста

---
+++ src/main/java/ru/job4j/exercises/Article.java
@@ -0,0 +1,30 @@
+package ru.job4j.exercises;
+
+/**
+ * Class Article проверка текста на оригинальность
+ *
+ * @author Ruslan Shuyski
+ * @version 3
+ */
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        String[] str1 = origin.split(" "); // создаём массив строк из текстов
+        String[] str2 = line.split(" ");
+        int q = 0; // счётчик равенства
+        for (String s : str2) {
+            for (String s1 : str1) {
+                if ((s.equals(s1))
+                        || ((s + ".").equals(s1))
+                        || ((s + ",").equals(s1))
+                        || ((s + "!").equals(s1))) {
+                    // проверяем на равенство слова
+                    // с учётом знаков пунктуации
+                    q++; // если равны увеличиваем счётчик
+                    break;
+                }
+            }
+        }
+        return q == str2.length; // если счётчик равен длине массива
+        // второго текста, то истина
+    }
+}
