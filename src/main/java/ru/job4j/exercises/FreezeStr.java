fe76f46f32656c7d4f907dad411cf9b95b02e594
shuyski <ryslan.shuyski@mail.ru>
2021-04-07 19:07:41 +0300

    2. Проверить две строчки на идентичность

--- src/main/java/ru/job4j/exercises/FreezeStr.java
+++ src/main/java/ru/job4j/exercises/FreezeStr.java
@@ -1,33 +1,25 @@
 package ru.job4j.exercises;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class FreezeStr проверка строк на идентичность
  *
  * @author Ruslan Shuyski
- * @version 1
+ * @version 2
  */
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        char[] str = new char[left.length()];
-                 // создаём два массива размером длин строк
-        char[] str1 = new char[right.length()];
-        for (int i = 0; i < left.length(); i++) {
-            str[i] = left.charAt(i);
-                             // добавляем строки посимвольно в наши массивы
-            str1[i] = right.charAt(i);
+        return count(left).equals(count(right));
+    }
+
+    public static Map<Character, Integer> count(String line) {
+        Map<Character, Integer> change = new HashMap<>();
+        for (char b : line.toCharArray()) {
+            Integer value = change.get(b);
+            change.put(b, value == null ? 1 : ++value);
         }
-        int q = 0; // счётчик равентства
-        for (int j = 0; j < str.length; j++) {
-            for (int i = 0; i < str1.length; i++) {
-                if (str[i] == str1[j]) {
-                    /* если символы равны заменяем символ внутреннего цикла на '0',
-                    чтобы он не учитывался в равентсве при дальнейшем сравнении */
-                    str[i] = '0';
-                    q = q + 1; // увеличиваем счётчик
-                    break;
-                }
-            }
-        }
-        return q == left.length(); // если счётчик равен длине строки, то истина
+        return change;
     }
 }
