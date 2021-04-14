6246a51953eef7e3bdd7dcf7b63c5abfeaaa799d
shuyski <ryslan.shuyski@mail.ru>
2021-04-14 11:40:57 +0300

    0.1. FileOutputStream. [#459949]

---
+++ src/main/java/ru/job4j/inout/Matrix.java
@@ -0,0 +1,38 @@
+package ru.job4j.inout;
+
+import java.io.FileOutputStream;
+
+/**
+ * Class Matrix создаёт таблицу умножения
+ * и записывает её в файл
+ *
+ * @author Ruslan Shuyski
+ * @version 1
+ */
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i - 1][j - 1] = i * j;
+            }
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = Matrix.multiple(5);
+        StringBuilder text = new StringBuilder(" ");
+        for (int i = 0; i < matrix.length; i++) {
+            text = new StringBuilder(i > 0 ? text + "\n" : "");
+            for (int j = 0; j < matrix.length; j++) {
+                text.append(matrix[i][j]).append(" ");
+            }
+        }
+        try (FileOutputStream out = new FileOutputStream("MatrixIt.txt")) {
+            out.write(text.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
