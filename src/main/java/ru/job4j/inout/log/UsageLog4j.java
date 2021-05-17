4d7f9aafdd2dfd8c9b5a1927f6a15556f542986d
ruslan <ryslan.shuyski@mail.ru>
2021-05-17 17:46:41 +0300

    3. Slf4j - вывод переменных. [#459943]

--- src/main/java/ru/job4j/inout/log/UsageLog4j.java
+++ src/main/java/ru/job4j/inout/log/UsageLog4j.java
@@ -5,13 +5,27 @@ import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory
+            .getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Ruslan Shuyski";
+        int age = 27;
+        char gender = 'M';
+        float height = 183.5F;
+        long dist = 50000;
+        double pi = Math.PI;
+        boolean pro = true;
+        short weight = 1500;
+        byte child = 1;
+        LOG.debug("Имя : {}, "
+                + " возраст : {},"
+                + " рост : {},"
+                + " пол : {},"
+                + " расстояние от дома до работы в метрах : {},"
+                + " число пи : {}, программист? : {},"
+                + " вес авто в килограммах : {}, "
+                + " оличество детей : {}",
+                name, age, height, gender, dist, pi, pro, weight, child);
     }
 }
