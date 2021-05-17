17fe2325420f9cc1edaeb3065b3736ec76704c28
ruslan <ryslan.shuyski@mail.ru>
2021-05-17 15:30:51 +0300

    1. Бот [#459972]

--- src/main/java/ru/job4j/inout/socket/EchoServer.java
+++ src/main/java/ru/job4j/inout/socket/EchoServer.java
@@ -10,7 +10,7 @@ import java.net.Socket;
  * ServerSocket
  *
  * @author Ruslan Shuyski
- * @version 1
+ * @version 2
  */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -21,21 +21,20 @@ public class EchoServer {
                     try (BufferedReader in = new BufferedReader(
                             new InputStreamReader(socket.getInputStream()))) {
                         String str = " ";
-                        while (!str.isEmpty()) {
+                        String answer = "What";
+                        while (!(str.isEmpty())) {
                             str = in.readLine();
                             System.out.println(str);
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                            if (str.contains("Hello")) {
-                                out.write("Hello, dear friend.\r\n".getBytes());
-                                break;
-                            }
                             if (str.contains("Exit")) {
                                 server.close();
-                                break;
+                                answer = "";
                             }
-                            out.write("What\r\n".getBytes());
-                            str = "";
+                            if (str.contains("Hello")) {
+                                answer = "Hello, dear friend.";
+                            }
                         }
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write(answer.getBytes());
                     }
                 }
             }
